15d0f1d78ef1caa9fbbab5ffb9da46853bf89f9c
MahesaGerrardybhumi <mahesagerrardybhumi@gmail.com>
2025-09-24 16:20:48 +0700

    tutor 4

--- main/views.py
+++ main/views.py
@@ -12,6 +12,24 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
